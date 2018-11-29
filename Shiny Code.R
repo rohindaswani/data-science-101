fb9189bf7659be02d7b1d0ad6d5bfec031926a2a
bambatman <45370361+bambatman@users.noreply.github.com>
2018-11-28 19:26:10 -0800

    Add files via upload

---
+++ Shiny Code.R
@@ -0,0 +1,70 @@
+# Build data into Shiny plot
+
+
+library(ggplot2)
+library(shiny)
+library(plotly)
+
+
+supply = read.csv('Fport September.csv')
+
+data(supply)
+nms <- names(supply)
+
+ui <- fluidPage(
+  
+  headerPanel("Supply"),
+  sidebarPanel(
+    sliderInput('sampleSize', 'Sample Size', min = 1, max = nrow(supply),
+                value = 5000, step = 1000, round = 0),
+    selectInput('x', 'X', choices = nms, selected = "squad"),
+    selectInput('y', 'Y', choices = nms, selected = "omd_dollar"),
+    selectInput('color', 'Color', choices = nms, selected = "tradelane"),
+    
+    selectInput('facet_row', 'Facet Row', c(None = '.', nms), selected = "tradelane"),
+    selectInput('facet_col', 'Facet Column', c(None = '.', nms)),
+    sliderInput('plotHeight', 'Height of plot (in pixels)', 
+                min = 100, max = 2000, value = 1000)
+  ),
+  mainPanel(
+    plotlyOutput('trendPlot', height = "900px")
+  )
+)
+
+server <- function(input, output) {
+  
+  #add reactive data information. Dataset = built in supply data
+  dataset <- reactive({
+    supply[sample(nrow(supply), input$sampleSize),]
+  })
+  
+  output$trendPlot <- renderPlotly({
+    
+    # build graph with ggplot syntax
+    
+    
+    #fac <- with(supply, reorder(supply$name, supply$dollar, mean, order = TRUE))
+    #supply$name <- factor(supply$name, levels = levels(fac))
+    
+    #p <- ggplot(dataset(), aes_string(x = input$x, y = input$y, color = input$color)) + 
+    #geom_boxplot() + theme(axis.text.x=element_text(size=6,angle=90))
+
+    p <- ggplot(supply, aes_string(input$x, input$y, color = input$color)) + 
+      geom_boxplot() + theme(axis.text.x=element_text(size=6,angle=90)) + xlab(input$x) + ylab(input$y)
+    
+    #p <- ggplot(dataset(), aes_string(x = input$x, y = input$y, color = input$color)) + 
+    #geom_boxplot() + theme(axis.text.x=element_text(size=6,angle=90))
+    
+    
+    # if at least one facet column/row is specified, add it
+    facets <- paste(input$facet_row, '~', input$facet_col)
+    if (facets != '. ~ .') p <- p + facet_grid(facets)
+    
+    ggplotly(p) %>% 
+      layout(height = input$plotHeight)
+    
+  })
+  
+}
+
+shinyApp(ui, server)
